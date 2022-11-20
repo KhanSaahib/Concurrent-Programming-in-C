28b8dfa476cea37798ba62f484298fb45257818b
Khan Sahib <saddamkhanashna786@gmail.com>
2022-11-20 19:10:46 +0200

    count_Mutex completed.

--- count_mutex.c
+++ count_mutex.c
@@ -3,13 +3,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <pthread.h>
-#define NUM_THREADS      64
-int size = 100000000;
-int array[100000000];
+#define NUM_THREADS     2
+int size = 10000000;
+int array[10000000];
 int count;
 long i;
-//pthread_mutex_t m;
-//pthread_t threads[NUM_THREADS];
+pthread_mutex_t mutex;
 
 
 void CreatArray() {
@@ -20,15 +19,19 @@ void CreatArray() {
 }
 
 void* countOnes(int* index) {
-	count = 0; 
 	int dx = (int)*index; 
-	for (int i = dx; i < size/NUM_THREADS; i++) {
+	long sect = size/	NUM_THREADS;
+	long long myFirstI = dx*sect;
+	long long myLastI = myFirstI + sect;
+
+	for (int i = myFirstI; i < myLastI; i++) {
 		if (array[i] == 1) {
+			pthread_mutex_lock(&mutex);
 			count++; 
+			pthread_mutex_unlock(&mutex);
 		}
 	}
-	int* countptr = &count; 
-	return countptr; 
+	return NULL; 
 }
 
 int Ones () {
@@ -41,7 +44,14 @@ int Ones () {
     return num; 
 }
 
-
+void print () {
+	printf("\n");
+	for (int i = 0; i < size; i++) {
+		printf("%d ", array[i]);
+	}
+	
+	printf("\n");
+}
 
 int main() {
 	srand(0);
@@ -49,28 +59,27 @@ int main() {
 	clock_t start, end;
 	CreatArray();
 	pthread_t th[NUM_THREADS];
+	pthread_mutex_init(&mutex,NULL);
 
-
-    //for (int k = 0; k < 100; k++) {
+    for (int k = 0; k < 100; k++) {
 	start = clock();
 	for (int i = 0; i < NUM_THREADS; i++) {
 		int* a = (int*)malloc(sizeof(int)); 
-		//int a;
-		int section = size / NUM_THREADS; 
-		*a = i * section;
+		int section = (size / NUM_THREADS); 
+		*a = i;
 		if (pthread_create(&th[i], NULL, (void*)&countOnes, a)) {
 			perror("failed to creat thread"); 
 		}
 	}
-    int sum = 0; 
 	for (int i = 0; i < NUM_THREADS; i++) {
 		if (pthread_join(th[i], (void**)&res) != 0) {
 			perror("failed to join thread");
 		}
-        sum += *res; 
 	}
     end = clock();
 	double time_In_Seconds = ((double)end - start) / CLOCKS_PER_SEC;
+
+
 
     start = clock();
     int Actual_Number_of_Ones = Ones(); 
@@ -81,10 +90,14 @@ int main() {
     printf("\nActual Frequency of Ones: %20d", Actual_Number_of_Ones);
     printf("\nSequential time Elapsed: %20f", time_In_Seconds1);
     printf("\nNumber of Threads: %23d", NUM_THREADS);
-    printf("\nThread's One count: %25d", sum);
+    printf("\nThread's One count: %25d", count);
     printf("\nTotal Threads Time Elapsed(seconds): %f", time_In_Seconds);
-    printf("\nIs threads Result true: %18s", (Actual_Number_of_Ones == sum)? "Yes": "No");
-    //}
+    printf("\nIs threads Result true: %18s", (Actual_Number_of_Ones == count)? "Yes": "No");
+    count = 0;
+	}
 
+
+	
+	pthread_mutex_destroy(&mutex);
 	return 0; 
 }
